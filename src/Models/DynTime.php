8dea2cd4e1c47c35840462125045850e5cedbe5e
Jelte ten Holt <inotherwords@gmail.com>
2022-06-03 13:54:26 -0600

    added time input option

---
+++ src/Models/DynTime.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace TheRiptide\LaravelDynamicDashboard\Models;
+
+use TheRiptide\LaravelDynamicDashboard\Models\DynModel;
+
+class DynTime extends DynModel
+{
+    public $rules = 'required';
+    public $component = 'simple';
+    public $type = 'time';
+
+    protected $table = 'dyn_dates';
+
+    protected $casts = [
+        'content' => 'datetime',
+    ];
+
+
+    public function getContent()
+    {
+        return $this->content
+            ? $this->content->format('H:i')
+            : null;
+    }
+}
