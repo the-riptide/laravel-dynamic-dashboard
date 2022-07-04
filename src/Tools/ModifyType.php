8d0d4eb35011e14042d9e32c218770e8fdbd557c
Jelte ten Holt <inotherwords@gmail.com>
2022-07-04 11:28:50 -0600

    can add new fields to existing models

--- src/Tools/ModifyType.php
+++ src/Tools/ModifyType.php
@@ -10,6 +10,8 @@ class ModifyType {
     use GetType;
 
     private $previous;
+    private $fields;
+    private $models;
 
     public function run($type = null)
     {
@@ -18,15 +20,17 @@ class ModifyType {
             ->map(
                 function ($item) 
                 {
-                    if (! $item->modelsMatchFields()) $this->compareAndModify($item);
+                    if (! $item->modelsMatchFields()) $this->setup($item);
                 }
             );
     }
 
-    private function compareAndModify($item)
+    private function setup($item)
     {
         $previous = $item->head();
-        $fields = $item->fields()->mapWithKeys(function ($field, $key) {
+
+        $this->models = $item->models();
+        $this->fields = $item->fields()->mapWithKeys(function ($field, $key) {
             return [
                 $key => [
                     'type' => $field['type'], 
@@ -35,23 +39,47 @@ class ModifyType {
             ];
         });
 
-        $field = $fields->shift();
+        $this->compare($this->fields->shift(), $this->models->shift(), $previous);
 
-        foreach ($item->models() as $model)
-        {       
-            dump(['model' => $model, 'field' => $field]);
-            if ($field && $model->name == $field['name'] && 'Dyn' . Str::ucfirst($field['type']) == class_basename($model))
-            {
-                $this->previous = $model;
-                $field = $fields->shift();
-            }
-            else {
-                if (! isset($fields[$model->name])) $previous = $this->removeModel($previous, $model);
-                if (isset($fields[$model->name])) {
-                    $previous = $this->insertModel($previous, $model, $field);
-                }
-            }    
+        // foreach ($item->models() as $model)
+        // {       
+            // dump(['model' => $model, 'field' => $field]);
+            // if ($field && $model->name == $field['name'] && 'Dyn' . Str::ucfirst($field['type']) == class_basename($model))
+            // {
+            //     $this->previous = $model;
+            //     $field = $fields->shift();
+            // }
+            // else {
+            //     if (! isset($fields[$model->name])) $previous = $this->removeModel($previous, $model);
+            //     if (isset($fields[$model->name])) {
+            //         $previous = $this->insertModel($previous, $model, $field);
+            //     }
+            // }    
+        // }
+    }
+
+    private function compare($field, $model, $previous)
+    {
+        if ($field && $model->name == $field['name'] && 'Dyn' . Str::ucfirst($field['type']) == class_basename($model))
+        {
+            $previous = $model;
+            $field = $this->fields->shift();
+            $model = $this->models->shift();
         }
+        else {
+            if (! isset($this->fields[$model->name])) 
+            {
+                $previous = $this->removeModel($previous, $model);
+                $model = $this->models->shift();
+            }
+            elseif (isset($this->fields[$model->name])) 
+            {
+                $previous = $this->insertModel($previous, $model, $field);
+                $field = $this->fields->shift();
+            }
+        }
+
+        if ($field != null && $model != null) $this->compare($field, $model, $previous);
     }
 
     private function removeModel($previous, $current)
